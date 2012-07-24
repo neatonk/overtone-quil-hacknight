b8884085dc25ac4e52aff7bd7555b4f59633efb2
Kevin Neaton <neatonk@gmail.com>
2012-07-23 20:48:18 -0500

    added project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject overtone-quil-hacknight "0.0.1-SNAPSHOT"
+  :description "Overtone + Quil examples."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.7.1"]
+                 [quil "1.6.0"]])
